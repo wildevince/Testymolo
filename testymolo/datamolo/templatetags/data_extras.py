0ea4dee7c12a49b89d705422267370ddb329ce9d
wildevince <wildevince@gmail.com>
2023-06-12 19:24:48 +0200

    3e assay : templatetags

--- testymolo/datamolo/templatetags/data_extras.py
+++ testymolo/datamolo/templatetags/data_extras.py
@@ -1,6 +1,5 @@
 from django import template
 from django.http import JsonResponse
-from datamolo.models import Subseq
 
 register = template.Library()
 
@@ -19,4 +18,19 @@ def subseq_line_xpos(subseq, protein_len:int):
     return (subseq["end"]) / protein_len * 100
 
 
+chnext:bool = False
+chlvl:int = 0
+width_min = 350
+
+
+@register.filter(is_safe=True)
+def generate_svg(data:dict):
+    L = data["protein"]["length"]
+
+    def each(subseq):
+        html = ""
+        html += f"<rect class='subseq' id={subseq['id']} height='20%' y='40%' fill='blue' fill-opacity='0.2' "
+        w = subseq['length']/ L *100
+        x = subseq['end']/L*100
+        html += f"x='{x}%' width='{w}%'"
 
